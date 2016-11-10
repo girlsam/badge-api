801bab4022b2da0c734513188fe333338c4a12c8
Sam Goldsmith <samggold@gmail.com>
2016-11-10 08:56:53 -0700

    update to GET routes

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -8,12 +8,47 @@ class AttendeesController < ApplicationController
     render json: @attendees
   end
 
-  # Show an attendees
-  # GET attendees/1
-  # GET /attendees/1.json
+  # Show an attendee
+  # GET /attendees/:id
   def show
     @attendee = Attendees.find(params[:id])
     render json: @attendee
+  end
+
+  # GET attendees/new
+  def new
+    @attendee = Attendees.new
+    render json: @attendees
+  end
+
+  # GET attendee/:id/edit
+  def edit
+  end
+
+  # POST /attendees
+  def create
+    @attendee = Attendees.new(attendee_params)
+
+    respond_to do |format|
+      if @attendee.save
+        msg = {:status => "ok", :message => "Success!" }
+        format.json { render :json => msg }
+      else
+        format.json { render json: @attendee.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_attendee
+    @attendee = Attendees.find(params[:id])
+  end
+
+  # only allow these parameters through
+  def attendee_params
+    params.require(:attendee).permit(:first_name, :last_name, :DOB, :image, :organization, :title)
+
   end
 
 end
